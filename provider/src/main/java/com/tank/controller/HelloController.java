2fa49b62b50540d60b585bf752de3057eabdc5b5
fuchun198435163.com <tank198435@163.com>
2020-07-06 10:32:01 +0800

    [fuchun] #refactor

--- provider/src/main/java/com/tank/controller/HelloController.java
+++ provider/src/main/java/com/tank/controller/HelloController.java
@@ -1,15 +1,23 @@
 package com.tank.controller;
 
+import com.tank.api.constants.UrlPrefix;
+import com.tank.api.protocol.ResponseBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static com.tank.api.constants.UrlPrefix.URL_PREFIX_FOR;
 
 /**
  * @author tank198435163.com
  */
 @RestController
-@RequestMapping(URL_PREFIX_FOR)
+@RequestMapping(UrlPrefix.URL_PREFIX_FOR)
 public class HelloController {
 
+  @GetMapping("/sayHello")
+  public ResponseEntity<ResponseBody> sayHello() {
+    ResponseBody payload = new ResponseBody();
+    payload.addValue("consumer", "hello,consumer");
+    return ResponseEntity.ok(payload);
+  }
 }
